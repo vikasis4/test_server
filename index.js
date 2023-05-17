5468b4e2600ee802195167fa7b361c854daf1670
vikas <vikasisgen@gmail.com>
2023-05-17 06:22:48 +0530

    brooo

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser());
+const server = require('http').createServer(app);
+
 
 app.post('/pay', (req, res) => {
     console.log(req.body);
+    res.json({true:'success'})
 })
 app.get('/', (req, res) => {
     res.json({ status: 'working' })
 })
+
 console.log('Hi');
-app.listen(9000, () => {
+
+server.listen(9000, () => {
     console.log('Servr is running on PORT 9000');
 })
