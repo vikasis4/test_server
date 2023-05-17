28a33fbaa935dbddfba91a4b0b81fd807425f6f8
vikas <vikasisgen@gmail.com>
2023-05-17 05:58:28 +0530

    normies

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/pay', (req, res) => {
     console.log(req.body);
 })
-app.get('/test', (req, res) => {
-    res.json({status:'working'})
+app.get('/', (req, res) => {
+    res.json({ status: 'working' })
 })
 console.log('Hi');
-app.listen(9000, ()=>{
+app.listen(9000, () => {
     console.log('Servr is running on PORT 9000');
 })
