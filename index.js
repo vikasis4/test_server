83a9a9a0aaf0d9d2582cde3b87b93ab8db22071d
vikas <vikasisgen@gmail.com>
2023-05-17 05:53:26 +0530

    normie

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', (req, res) => {
     console.log(req.body);
 })
+app.get('/test', (req, res) => {
+    res.json({status:'working'})
+})
 
 app.listen(9000, ()=>{
     console.log('Servr is running on PORT 9000');
