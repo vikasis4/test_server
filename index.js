ab7caade0a7d8fa000d765962aa6657b589caaca
vikas <vikasisgen@gmail.com>
2023-05-17 06:06:21 +0530

    goDad

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/pay', (req, res) => {
+app.post('/pay', (req, res) => {
     console.log(req.body);
 })
 app.get('/', (req, res) => {
